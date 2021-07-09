e8cb3b51996bddbf25d6ba8ee3a78fb9e60381d6
qkohst <7311626-qkohst@users.noreply.gitlab.com>
2021-07-10 02:14:26 +0700

    first commit + login_page

---
+++ lib/login_page.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:http/http.dart' as http;
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _State();
+}
+
+class _State extends State<LoginPage> {
+  TextEditingController txtUsername = TextEditingController();
+  TextEditingController txtPassword = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+          child: Text(
+            'LOGIN',
+            style: TextStyle(
+                color: Colors.blue, fontWeight: FontWeight.w500, fontSize: 30),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
+          child: Text(
+            'E-Skripsi Unirow Tuban',
+            style: TextStyle(
+                color: Colors.blue, fontWeight: FontWeight.w500, fontSize: 20),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(10),
+          child: TextField(
+            controller: txtUsername,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Username',
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+          child: TextField(
+            obscureText: true,
+            controller: txtPassword,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Password',
+            ),
+          ),
+        ),
+        Container(
+            height: 70,
+            width: double.infinity,
+            padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+            child: RaisedButton(
+              textColor: Colors.white,
+              color: Colors.blue,
+              child: Text('LOGIN'),
+              onPressed: () {
+                this._doLogin();
+              },
+            )),
+      ],
+    ));
+  }
+
+  Future _doLogin() async {
+    if (txtUsername.text.isEmpty || txtPassword.text.isEmpty) {
+      Alert(
+              context: context,
+              title: "Masukkan Username dan Password",
+              type: AlertType.warning)
+          .show();
+      return;
+    }
+
+    final response = await http
+        .post(Uri.parse('http://192.168.43.71:8000/api/v1/auth/login'), body: {
+      'api_key': '4pyHNMVxTnNVpe571jMMXtE0a5X74PDYqkoLRds9lXlSqOW7GtqrBT29ptY5',
+      'username': txtUsername.text,
+      'password': txtPassword.text,
+    }, headers: {
+      'Accept': 'application/json'
+    });
+
+    if (response.statusCode == 200) {
+      Alert(context: context, title: "Login Berhasil", type: AlertType.success)
+          .show();
+    } else {
+      Alert(
+              context: context,
+              title: "Username atau Password Salah",
+              type: AlertType.error)
+          .show();
+    }
+  }
+}
